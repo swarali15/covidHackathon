dece9ee12a7dbef46a60d744ad754f350062f317
swarali15 <swarali.purandare18@vit.edu>
2020-05-02 07:32:10 +0530

    Updates

---
+++ precache-manifest.4109984c07b4bb7c657ef1175725e491.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18763df471a6c4ddd90ca9f0bdcf14f8",
+    "url": "/covidHackathon/index.html"
+  },
+  {
+    "revision": "7f52264ce9be4f89998a",
+    "url": "/covidHackathon/static/css/main.6ef01daf.chunk.css"
+  },
+  {
+    "revision": "6e68201e633756eb568a",
+    "url": "/covidHackathon/static/js/2.51e00660.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/covidHackathon/static/js/2.51e00660.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f52264ce9be4f89998a",
+    "url": "/covidHackathon/static/js/main.836d2a0a.chunk.js"
+  },
+  {
+    "revision": "8263c8f753aa58b79e53",
+    "url": "/covidHackathon/static/js/runtime-main.c17a2431.js"
+  },
+  {
+    "revision": "4ee2c6b22fb64722a7496a9159080dee",
+    "url": "/covidHackathon/static/media/ACTIVECOVID19.4ee2c6b2.PNG"
+  },
+  {
+    "revision": "3fc34a02f2f4fafc7cdfd0664068531c",
+    "url": "/covidHackathon/static/media/foodbank.3fc34a02.jpeg"
+  },
+  {
+    "revision": "27e646c1958bddbb7988a601a33f8a0e",
+    "url": "/covidHackathon/static/media/migrantworkers.27e646c1.PNG"
+  },
+  {
+    "revision": "31b4e9f8286d7dd863961ff247048c6f",
+    "url": "/covidHackathon/static/media/populationdensity.31b4e9f8.PNG"
+  },
+  {
+    "revision": "b805012988f75f88c5b78e8345fa9ed7",
+    "url": "/covidHackathon/static/media/totalagricultureproduction.b8050129.PNG"
+  }
+]);
